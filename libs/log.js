a6746184144cdb4966147de51e5c81b578530e12
Luan Fabiano <luan.pablo@luizalabs.com>
2018-06-26 14:24:31 -0300

    Add .nvmrc

--- libs/log.js
+++ libs/log.js
@@ -1,27 +1,25 @@
-'use strict';
 module.exports = function (bunyanLogger) {
   return function * (next) {
-    const startDate = new Date();
-    let error;
-    
-    yield next;
-    
-    const endDate = new Date();
-    const latency = parseFloat(parseFloat((endDate - startDate) / 1000).toFixed(3));
+    const startDate = new Date()
+
+    yield next
+
+    const endDate = new Date()
+    const latency = parseFloat(parseFloat((endDate - startDate) / 1000).toFixed(3))
     let log = {
-      "http.lantency_seconds": latency,
-      "path": this.path,
-      "http.request_body": typeof(this.request.body) === 'string' ? this.request.body : JSON.stringify(this.request.body),
-      "http.request_header": JSON.stringify(this.request.headers),
-      "http.request_method": this.request.method,
-      "http.response_body": typeof(this.response.body) === 'string' ? this.response.body : JSON.stringify(this.response.body),
-      "http.response_header": JSON.stringify(this.response.headers),
-      "http.request_size": 1,
-      "http.status_code": this.response.status,
-      "http.url": this.request.href,
+      'http.lantency_seconds': latency,
+      'path': this.path,
+      'http.request_body': typeof (this.request.body) === 'string' ? this.request.body : JSON.stringify(this.request.body),
+      'http.request_header': JSON.stringify(this.request.headers),
+      'http.request_method': this.request.method,
+      'http.response_body': typeof (this.response.body) === 'string' ? this.response.body : JSON.stringify(this.response.body),
+      'http.response_header': JSON.stringify(this.response.headers),
+      'http.request_size': 1,
+      'http.status_code': this.response.status,
+      'http.url': this.request.href,
       'type': 'json'
-    };
-    
-    bunyanLogger.info(log);
-  };
-};
+    }
+
+    bunyanLogger.info(log)
+  }
+}
--- libs/response-time.js
+++ libs/response-time.js
@@ -1,7 +1,7 @@
-'use strict';
-module.exports = function* (next) {
-  const startDate = new Date();
-  yield next;
-  const ms = new Date() - startDate;
-  this.set('X-Response-Time', `${ms}ms`);
-};
+'use strict'
+module.exports = function * (next) {
+  const startDate = new Date()
+  yield next
+  const ms = new Date() - startDate
+  this.set('X-Response-Time', `${ms}ms`)
+}
