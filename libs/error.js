83b066e9d58674183e4e7de87bcfde2175e707ef
Pedro Henrique F. Dias <pedroarapua@hotmail.com>
2018-07-22 19:17:45 -0300

    started crated sentry middleware

---
+++ libs/error.js
@@ -0,0 +1,22 @@
+'use strict';
+const Boom = require('boom');
+const _ = require('lodash');
+
+module.exports = function *(next){
+  try {
+    yield next;
+    
+    if(this.status == 404) {
+      this.throw(404);  
+    }
+  } catch (err) {
+    this._sentryError = err;
+    this.status = err.isJoi ? 400 : err.isBoom ? err.output.statusCode : (err.status || 500);
+    this.body = err.isBoom ? err.output.payload : Boom.boomify(err, { statusCode: this.status }).output.payload;
+    if(err.isJoi) {
+      this.body.data = _.map(Array.isArray(err.details) ? err.details : [err.details], function(obj) {
+        return _.pick(obj, 'message', 'path');
+      });
+    }
+  }
+};
--- libs/index.js
+++ libs/index.js
@@ -2,10 +2,14 @@ const KoaBunyanLogger = require('./log');
 const KoaHeaderResponseTime = require('./response-time');
 const KoaParseSequelize = require('./parse-sequelize');
 const KoaNewRelic = require('./newrelic');
+const KoaSentry = require('./sentry');
+const KoaError = require('./error');
 
 module.exports = {
   KoaBunyanLogger,
   KoaHeaderResponseTime,
   KoaParseSequelize,
-  KoaNewRelic
+  KoaNewRelic,
+  KoaSentry,
+  KoaError
 };
